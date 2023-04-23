c5eaed97ec6bfd0ce327fb16c299dea24d9a3e4d
Thermax1 <am15943n@pace.edu>
2023-04-23 14:50:11 -0400

    assignment

--- munoz-blog/src/App.js
+++ munoz-blog/src/App.js
@@ -4,8 +4,11 @@ import { useEffect } from 'react';
 import './App.css';
 
 function Blog() {
+  //posts stores all blog posts, like a DB for the assignment. currentPost is for the one that's clicked
   const [posts, setPosts] = useState([]);
+  const [currentPost, setCurrentPost] = useState(null);
   // useEffect allows us to fetch data, update dom, for AJAX etc
+  // The arrow function => is a faster way to call a function in a way. Left side is input, right side is output of function. Trial and error to figure this out.
   useEffect(() => {
     const xmlStart = new XMLHttpRequest();
     xmlStart.open('GET', '/my_blogs.xml');
@@ -13,6 +16,7 @@ function Blog() {
       const parser = new DOMParser();
       const xmlDoc = parser.parseFromString(xmlStart.responseText, 'text/xml');
       const postElements = xmlDoc.getElementsByTagName('post');
+      //Turn postElements into an array, making it easier to show
       const postsArray = Array.from(postElements).map(postElement => ({
         id: postElement.getAttribute('id'),
         date: postElement.getElementsByTagName('date')[0].textContent,
@@ -23,35 +27,44 @@ function Blog() {
         text: postElement.getElementsByTagName('text')[0].textContent,
         body: postElement.getElementsByTagName('body')[0].textContent,
       }));
+      // Now the posts are set with the data that's been Arrayed
       setPosts(postsArray);
       };
       xmlStart.send();
     }, []);
 
     return (
+      // The ? means to show full post if it's there. The : means to show the list of Blog Posts
       <div>
-        {posts.map(post => (
-          <div key={post.id}>
-            <h1>{post.title}</h1>
-            <h3>{post.author}</h3>
-            <h4>{post.date}</h4>
-            <img src={post.imagePath} alt='photos'/>
-            <p>{post.text}</p>
-            <p>{post.body}</p>
-
-            
+        {currentPost ? (
+          <div>
+            <h1>{currentPost.title}</h1>
+            <h3>{currentPost.author}</h3>
+            <h4>{currentPost.date}</h4>
+            <img src={currentPost.imagePath} alt='A unique image' />
+            <p>{currentPost.body}</p>
+            <button onClick={() => setCurrentPost(null)}>Back to Home</button>
           </div>
-        ))}
-      </div>
+        ) : (
+          posts.map(post => (
+            <div key={post.id}>
+              <h1><a href='#' onClick={() => setCurrentPost(post)}>{post.title}</a></h1>
+              <h3>{post.author}</h3>
+              <h4>{post.date}</h4>
+              <img src={post.imagePath} alt = 'Blog Photo here' />
+              <p>{post.text}</p>
+            </div>
+          ))
+        )}
+        </div>
     );
   }
-
 function Nav() {
   const [articles, setArticles] = useState([]);
-
+// Similar HttPRequest to get the News articles. Better than fetch for me.
   useEffect(() => {
     const xmlStart = new XMLHttpRequest();
-    xmlStart.open('GET' , 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=');
+    xmlStart.open('GET' , 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=69b9f4c3a32d4b719905990186c26184');
     xmlStart.onload = () => {
       const jsonData = JSON.parse(xmlStart.responseText);
       setArticles(jsonData.articles);
@@ -59,8 +72,9 @@ function Nav() {
     xmlStart.send();
 
     const timeID = setInterval(() => {
+      // Console log is self-check to see if the page updates and it does.
       console.log('Updating the news');
-      xmlStart.open('GET' , 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=');
+      xmlStart.open('GET' , 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=69b9f4c3a32d4b719905990186c26184');
       xmlStart.send();
     }, 60000);
 
@@ -71,10 +85,9 @@ function Nav() {
 
   return (
     <div>
-    <a href='/index.html' target='blank'>Back to homepage here!</a>
       <h2>The Headlines</h2>
       <ul>
-        {articles.map((article) => (
+        {articles.slice(0, 6).map((article) => (
           <li key={article.title}>
             <a href={article.url} target='blank'>{article.title}</a>
           </li>
@@ -85,12 +98,12 @@ function Nav() {
 }
 
 export default function App() {
-  
+  // This would render the App when the info is passed to Index.js, which shows up on index.html
   return (
     <div className='container'>
       <header id='header'>Welcome to My Page</header>
 
-      <div className='columns'> {/* create the columns */}
+      <div className='columns'> {/* create the columns for the page */}
         <div className='nav'>
         <Nav />
         </div>
